9c38ad6a810887248d5e6904bee2a1f480da846d
Anubhav7890 <anubhavsingh@appscrip.co>
2024-10-26 18:27:59 +0530

    Title:Fixed Deploy Error Dev:Anubhav Date:26-10-2-24 Description Fixed Deploy Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,2 @@
 // next.config.js
-module.exports = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Necessary for GitHub Pages to handle images
-  },
-  // Optional: specify the base path if your project is not at the root
-  basePath: '/insurance-blog-website',
-};
+module.exports = {};
