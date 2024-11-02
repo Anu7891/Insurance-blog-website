b5284a2e3baae4c62b33ad0a81a5a542de26cdff
Anubhav7890 <anubhavsingh@appscrip.co>
2024-11-02 13:15:58 +0530

    Title: Optimize some code Date:27-10-2024 Dev: Anubhav Description: Removed Console

--- utils/imageConvertToWebp/convert-to-webp.jsx
+++ utils/imageConvertToWebp/convert-to-webp.jsx
@@ -1,7 +1,6 @@
 // Function to convert image source to WebP format
 export const convertToWebP = (src) => {
 
-    console.log(src,"sfshgfddgh")
     if (src) {
         const extensionRegex = /\.(png|jpg|jpeg)$/i;
         if (extensionRegex.test(src)) {
